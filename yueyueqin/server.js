07b12998cb76b35b3cf4b80ffd2fd7428acdecfb
Yueyue07 <xww.qyy@gmail.com>
2016-01-23 14:04:36 -0800

    add actor api in the route

--- yueyueqin/server.js
+++ yueyueqin/server.js
@@ -3,9 +3,12 @@ const app = module.exports = exports = express();
 const mongoose = require('mongoose');
 const moviesRouter = require(__dirname + '/routes/movies_routes');
 const nonapiRouter = require(__dirname + '/routes/nonapi_routes');
+const actorsRouter = require(__dirname + '/routes/actors_routes');
 mongoose.connect(process.env.MONGOLAB_URI||'mongodb://localhost/movies_app_dev');
 
 app.use('/api',moviesRouter);
+
+app.use('/api', actorsRouter);
 
 app.use('/', nonapiRouter);
 
